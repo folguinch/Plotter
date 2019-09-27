25013283be0abb1c8e717ccd196aeb7d48bc3c9e
Fernando Olguin <f.olguin.ch@gmail.com>
2019-09-27 17:20:54 +0800

    New marker property

--- loaders.py
+++ loaders.py
@@ -1,5 +1,7 @@
 import os
 
+from src.utils import auto_vminmax
+
 def load_cube(args):
     from spectral_cube import SpectralCube
     if args.cubename:
@@ -18,6 +20,53 @@ def load_fits(args):
 
     return args
 
+def load_overplot(args):
+    if args.overplot:
+        if args.oplabel:
+            if len(args.oplabel)==1:
+                labels = args.oplabel*len(args.overplot)
+            else:
+                assert len(args.oplabel)==len(args.overplot)
+                labels = args.oplabel
+        else:
+            labels = [None]*len(args.overplot)
+
+        if args.opcolor:
+            if len(args.opcolor)==1:
+                colors = args.opcolor*len(args.overplot)
+            else:
+                assert len(args.opcolor)==len(args.overplot)
+                colors = args.opcolor
+        else:
+            colors = ['w']*len(args.overplot)
+                
+        rmin, rmax = float('inf'), -float('inf')
+        over = []
+        for l,o,c in zip(labels, args.overplot, colors):
+            ext = os.path.splitext(o)[1]
+            label = None if l=='-' else l
+            d = {'label': label, 'colors': c}
+            if ext=='.fits':
+                img = get_fits(o)
+                over += [(img, d)]
+                vmin, vmax = get_img_ranges(img)
+                rmin = min(rmin, vmin)
+                rmax = max(rmin, vmax)
+            elif ext=='.dat':
+                over += [(get_dat(o), d)]
+            else:
+                raise NotImplementedError
+        args.overplots = [over, rmin, rmax]
+    return args
+
 def get_fits(filename):
     from astropy.io import fits
     return fits.open(os.path.expanduser(filename))[0]
+
+def get_dat(filename):
+    import numpy as np
+    return np.loadtxt(os.path.expanduser(filename))
+
+def get_img_ranges(img):
+    return auto_vminmax(img.data)
+
